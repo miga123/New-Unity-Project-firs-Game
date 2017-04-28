f3d20b7f76c0d099f4af3fab289d684e1539626b
DESKTOP-PIVSOU3\deXter <migajaparidze555@gmail.com>
2017-04-28 17:57:46 +0400

    new scrips

---
+++ Assets/script/CameFolower.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameFolower : MonoBehaviour
+{
+    public Transform target;
+    Vector3 distanceFromTarget;
+
+    
+    void Start()
+    {
+        distanceFromTarget = transform.position - target.position;
+
+    }
+
+
+    void Update()
+    {
+        transform.position = target.position + distanceFromTarget;
+    }
+}
---
+++ Assets/script/playermover.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermover : MonoBehaviour
+{
+    
+    public Transform player;
+    public Rigidbody playerBody;
+    public float SpeedMultuplayer;
+
+    Vector3 deltaPos;
+
+
+    void Start ()
+    {
+        deltaPos = Vector3.zero;
+	}
+	
+	
+	void Update ()
+    {
+        //Debug.Log (Input.GetKey(KeyCode.DownArrow) );
+
+        deltaPos.z = Input.GetAxis("Vertical");
+        deltaPos.x = Input.GetAxis("Horizontal");
+
+       
+
+
+       // Input.GetAxis("Horizontal"));
+
+
+        // player.position = player.position + deltaPos;
+        //short form
+       // transform.position += deltaPos;
+
+    }
+
+     void FixedUpdate()
+    {
+        playerBody.position = playerBody.position + deltaPos;
+        playerBody.AddForce(deltaPos * SpeedMultuplayer);
+    }
+
+}
