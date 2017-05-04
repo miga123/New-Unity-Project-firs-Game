60a18e975fc4b3eb656d0ee8fc74bf685c3381a6
miga japaridze <migajaparidze555@gmail.com>
2017-05-04 20:33:57 +0400

    first Finish 1.0

--- Assets/script/CoinCounter.cs
+++ Assets/script/CoinCounter.cs
@@ -4,10 +4,24 @@ using UnityEngine.UI;
 public class CoinCounter : MonoBehaviour {
 
     public int score = 0;
-    public Text scoreText;
     public string coinTagString;
     public string badCoinTagString;
+	public GameObject menu;
+	public playermover myMover;
+	public Rigidbody myBody;
 
+	public Text scoreText;
+	public Text resultText;
+	public string winText;
+	public string loseText;
+
+	int targetScore;
+
+	void Start()
+	{
+		targetScore = GameObject.FindGameObjectsWithTag(coinTagString).Length;
+		menu.SetActive (false);
+	}
 
     void OnCollisionEnter(Collision col)
     {
@@ -16,20 +30,40 @@ public class CoinCounter : MonoBehaviour {
 
     void OnTriggerEnter(Collider other)
     {
-
-        if (other.tag =="Coin")
+		if (other.tag == coinTagString)
         {
             score = score + 1;
         }
-        else if(other.tag == "BadCoin" && score != 0)
+		else if(other.tag == badCoinTagString)
         {
-                score = score - 1;
-            
-        }
-        
+			GameOver(false);    
+        }  
+
         Debug.Log("Triggered" + score + " " + other.gameObject.name);
+
+		if (targetScore == score) 
+		{
+			GameOver (true);
+		}
 
         scoreText.text = "Score:" + score;
         Destroy(other.gameObject);
     }
+
+	void GameOver(bool didwin)
+	{
+		menu.SetActive (true);
+		myMover.enabled = false;
+		myBody.isKinematic = true;
+
+		if (didwin) 
+		{
+			resultText.text = winText;
+		}
+		else 
+		{
+			resultText.text = loseText;
+		}
+
+	}
 }
---
+++ Assets/script/MenuButtuns.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtuns : MonoBehaviour 
+{
+
+	public void RestartGame()
+	{
+		SceneManager.LoadScene (0);
+	}
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+
+}
